75fde8f982c251932146da7767d9092f91fcd1a6
Aron Strandberg <aron.olof.strandberg@gmail.com>
2019-12-05 21:59:33 +0100

    Some minor changes to the day 4 solution

--- R/day-4.R
+++ R/day-4.R
@@ -1,23 +1,18 @@
 # --- Day 4: Secure Container ---
 
-library(dplyr)
-library(stringr)
-library(tidyr)
+library(tidyverse)
 
-input <- scan("input/input-day-4.txt", sep = "-")
-
-# Part 1:
-passwords <- tibble(input = input[1]:input[2]) %>%
-  mutate(input_string = str_pad(as.character(input), width = 6, pad = "0")) %>%
-  separate(input_string, sep = c(1:6), into = paste0("d", 1:6)) %>%
+# Part 1
+passwords <- tibble(input = 136760:595730) %>%
+  mutate_at(1, ~str_pad(., width = 6, pad = "0")) %>%
+  separate(1, paste(1:6), 1:6, remove = FALSE) %>%
   mutate_all(as.numeric) %>%
-  filter(d2>=d1, d3>=d2, d4>=d3, d5>=d4, d6>=d5) %>%
-  filter(d2==d1 | d3==d2 | d4==d3 | d5 == d4 | d6==d5)
+  filter(rowMeans(.[3:7] >= .[2:6]) == 1 & rowMeans(.[3:7] == .[2:6]) != 0)
 nrow(passwords)
 
-# Part 2:
+# Part 2
 passwords %>%
-  pivot_longer(col = d1:d6) %>%
+  pivot_longer(-input) %>%
   count(input, value) %>%
   filter(n == 2) %>%
   count(input) %>%
