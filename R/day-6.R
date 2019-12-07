af0efeb0a9674a870287adca94a3f5e2ad30db7a
Aron Strandberg <aron.olof.strandberg@gmail.com>
2019-12-07 17:53:38 +0100

    Update day-6.R

--- R/day-6.R
+++ R/day-6.R
@@ -1,26 +1,22 @@
+# --- Day 6: Universal Orbit Map ---
+
 library(tidyverse)
+input <- read_delim("input/input-day-6.txt", delim = ")", col_names = paste(1:2))
+df <- filter(input, `1` == "COM")
 
-df <- input <- read_delim("input/input-day-6.txt", delim = ")", col_names = paste(1:2))
-
-while(any(!is.na(df[ncol(df)]))) {
-  df <- left_join(df, setNames(input, ncol(df):(ncol(df)+1)))
+while(!all(is.na(df[[ncol(df)]]))) {
+  df <- left_join(df, setNames(input, ncol(df) + 0:1))
 }
 
-# Solution 1
+# --- Part One ---
 df %>%
-  filter_at(1, all_vars(.=="COM")) %>%
   pivot_longer(-1) %>%
-  filter_at("value", ~!is.na(.)) %>%
-  distinct() %>%
-  summarise(answer_1=sum(as.numeric(name))-nrow(.))
+  filter(!is.na(value), !duplicated(value)) %>%
+  summarise(sum(as.numeric(name) - 1))
 
-# Solution 2
+# --- Part Two ---
 df %>%
-  filter_at(1, all_vars(.=="COM")) %>%
-  filter_all(any_vars(.=="YOU" | .=="SAN")) %>%
-  t() %>%
-  as_tibble() %>%
+  filter_all(any_vars(. == "YOU" | . == "SAN")) %>% 
+  {as_tibble(t(.))} %>%
   filter(xor(is.na(V1), is.na(V2)) | V1 != V2) %>%
-  summarise(answer_2 = sum(!is.na(.)))-2
-
-
+  summarise(sum(!is.na(.)) - 2)
