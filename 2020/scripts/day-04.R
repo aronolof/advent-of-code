a642c494ae9fd3525279f3e14e9e267b45777219
Aron Strandberg <aron.olof.strandberg@gmail.com>
2020-12-07 01:32:12 +0100

    Simplify the validation

--- 2020/scripts/day-04.R
+++ 2020/scripts/day-04.R
@@ -10,20 +10,21 @@ sum(sapply(split(input, cumsum(input == "")),
            }))
 
 # Part 2
-passport_list <- lapply(split(input, cumsum(input == "")), function(x) unlist(strsplit(x, " ")))
+passport_list <- lapply(split(input, cumsum(input == "")),
+                        function(x) unlist(strsplit(x, " ")))
 
 sum(sapply(passport_list, function(x) {
   v <- sub("[a-z]{3}:(.*)", "\\1", x)
   names(v) <- sub("([a-z]{3}):.*", "\\1", x)
   
-  abs(as.numeric(v["byr"]) - 1920) <= 82 &
-    abs(as.numeric(v["iyr"]) - 2010) <= 10 &
-    abs(as.numeric(v["eyr"]) - 2020) <= 10 &
+  as.numeric(v["byr"]) %in% 1920:2002 &
+    as.numeric(v["iyr"]) %in% 2010:2020 &
+    as.numeric(v["eyr"]) %in% 2020:2030 &
     grepl("^[0-9]{9}$", v["pid"]) &
     v["ecl"] %in% c("amb", "blu", "brn", "gry", "grn", "hzl", "oth") &
     grepl("^#[0-9a-f]{6}$", v["hcl"]) &
-    abs(floor(prod(as.numeric(sub("^([0-9]*)(cm|in)$*", "\\1", v["hgt"])),
-                   (1 + grepl("^[0-9]*(in)$", v["hgt"]) * 1.55))) - 150) <= 43
+    floor(prod(as.numeric(sub("^([0-9]*)(cm|in)$*", "\\1", v["hgt"])),
+                   (1 + grepl("^[0-9]*(in)$", v["hgt"]) * 1.55))) %in% 150:193
   }),
   na.rm = TRUE)
 
