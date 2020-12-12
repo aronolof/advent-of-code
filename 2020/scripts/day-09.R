376eacdefe4ebb4e605472b6de985f7cea8fdb13
Aron Strandberg <aron.olof.strandberg@gmail.com>
2020-12-12 06:14:22 +0100

    Fix day 9 typo

--- 2020/scripts/day-09.R
+++ 2020/scripts/day-09.R
@@ -13,7 +13,7 @@ ans_1
 # Part 2
 tbl <- (function(x) x[x[[1]] < x[[2]],])(expand.grid(seq(input), seq(input)))
 
-for(i in seq(nrow(search_table_2))) {
+for(i in seq(nrow(tbl))) {
   range_sum <- sum(input[tbl[i,1]:tbl[i,2]])
     
   if(range_sum == ans_1) {
