83b4cf5fe92e4a7f72236c35d7ec681516fa5c8a
Aron Strandberg <aron.olof.strandberg@gmail.com>
2021-12-02 13:17:16 +0100

    Day 2 solution

---
+++ 2021/scripts/day-02.R
@@ -0,0 +1,10 @@
+# --- Day 2: Dive! ---
+
+input <- read.delim("2021/input/input-02.txt", sep = ' ', header = FALSE)
+
+# Part 1
+prod(aggregate(input$V2 * ifelse(input$V1 == 'up', -1, 1), by=list(input$V1 == 'forward'), FUN=sum)$x)
+
+# Part 2
+((input$V1 != 'forward') * ifelse(input$V1 == 'up', -1, 1) * input$V2) |>
+  (\(x) tail(cumsum((!x) * input$V2), 1) * tail(cumsum((!x) * input$V2 * cumsum(x)), 1))()
