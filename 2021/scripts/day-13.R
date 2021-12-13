a6860123293f48e1acc05fc8392bfc350ff6556f
Aron Strandberg <aron.olof.strandberg@gmail.com>
2021-12-13 16:53:53 +0100

    Simplify code

--- 2021/scripts/day-13.R
+++ 2021/scripts/day-13.R
@@ -1,14 +1,13 @@
 # --- Day 13: Transparent Origami ---
 
-input <- readLines('2021/input/input-13.txt')
+input <- readLines("2021/input/input-13.txt")
 
-# Part 1 and 2
-fold <- read.table(text = gsub('fold along ', '', input[grepl('^f', input)]), sep = '=')
-dots <- read.table(text = input[grepl('^[^f]', input)], sep = ',', col.names = c('x', 'y'))
+fold <- read.table(text = gsub("fold along ", "", input[grepl("^f", input)]), sep = "=")
+dots <- read.table(text = input[grepl("^\\d", input)], sep = ",", col.names = c("x", "y"))
 
-for(i in seq(nrow(fold))) {
-  dots[dots[fold[i,1]] > fold[i,2], fold[i,1]] <- 2*fold[i,2] - dots[dots[fold[i,1]] > fold[i,2], fold[i,1]]
-  if(i == 1) print(nrow(unique(dots)))
+for (i in 1:nrow(fold)) {
+  dots[fold[i, 1]] <- pmin(2 * fold[i, 2] - dots[fold[i, 1]], dots[fold[i, 1]])
+  if (i == 1) print(nrow(unique(dots))) # Part 1
 }
 
-plot(dots, ylim = rev(range(dots$x)))
+plot(dots, ylim = rev(range(dots$x))) # Part 2
