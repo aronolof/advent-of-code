52d46710ee4dd03ae287aef2dad2a67473930306
Aron Strandberg <aron.olof.strandberg@gmail.com>
2021-12-13 14:15:55 +0100

    Complete day 13 solution

--- 2021/scripts/day-13.R
+++ 2021/scripts/day-13.R
@@ -2,4 +2,13 @@
 
 input <- readLines('2021/input/input-13.txt')
 
-instructions <- input[grepl('fold', input)]
+# Part 1 and 2
+fold <- read.table(text = gsub('fold along ', '', input[grepl('^f', input)]), sep = '=')
+dots <- read.table(text = input[grepl('^[^f]', input)], sep = ',', col.names = c('x', 'y'))
+
+for(i in seq(nrow(fold))) {
+  dots[dots[fold[i,1]] > fold[i,2], fold[i,1]] <- 2*fold[i,2] - dots[dots[fold[i,1]] > fold[i,2], fold[i,1]]
+  if(i == 1) print(nrow(unique(dots)))
+}
+
+plot(dots, ylim = rev(range(dots$x)))
