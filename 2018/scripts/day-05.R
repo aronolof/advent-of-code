f0ec20657744b5a7d403934a968e150a0d1aacd3
Aron Strandberg <aron.olof.strandberg@gmail.com>
2022-11-29 20:32:24 +0100

    First attempt at part 2 solution

--- 2018/scripts/day-05.R
+++ 2018/scripts/day-05.R
@@ -2,27 +2,31 @@
 
 input <- readLines('2018/input/input-05')
 
-# Part 1
-# Loop version
-
-v <- utf8ToInt(input)
-
-for(i in 0:1000) {
-  compare = v[pmin(pmax(seq(v) + sign((i+seq(v)) %% 2-.5), 1), max(seq(v)))]
-  new_v <- v[abs(v - compare) != 32]
-  if(identical(v, new_v)) break
-  v <- new_v
-}
-nchar(intToUtf8(v))
-
-# Recursive function version
-(\(v, i = 0, no_change = F) {
+# Part 1 
+reaction <- \(v, i = 0, no_change = F) {
   compare = v[pmin(pmax(seq(v) + sign((i+seq(v)) %% 2-.5), 1), max(seq(v)))]
   new_v <- v[abs(v - compare) != 32]
   if(no_change & identical(v, new_v)) return(new_v)
   Recall(new_v, i+1, identical(v, new_v))
-})(utf8ToInt(input)) |>
-  intToUtf8() |>
-  nchar()
+}
 
+nchar(intToUtf8(reaction(utf8ToInt(input))))
+
+# Part 2
+shortest = nchar(intToUtf8(reaction(utf8ToInt(input))))
+for(x in 1:26) {
+  removed <- gsub(paste(letters[x], LETTERS[x], sep = '|'), '', input)
+  shortest <- min(
+    shortest,
+    nchar(intToUtf8(reaction(utf8ToInt(removed))))
+  )
+}
+shortest
+
+sapply(1:26, \(x) {
+  utf8ToInt(gsub(paste(letters[x], LETTERS[x], sep = '|'), '', input)) |>
+    reaction() |>
+    intToUtf8() |>
+    nchar()
+})
 
