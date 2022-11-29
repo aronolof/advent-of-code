ab6be0537d2a4cf65ff77fdcb39f64aff950333f
Aron Strandberg <aron.olof.strandberg@gmail.com>
2022-11-29 13:52:23 +0100

    Add day 2 solution

---
+++ 2018/scripts/day-02.R
@@ -0,0 +1,15 @@
+# --- Day 2: Inventory Management System ---
+input = readLines('2018/input/input-02')
+
+# Part 1
+prod(sapply(2:3, \(i) sum(sapply(input, \(x) max(table(strsplit(x, '')) == i)))))
+
+# Part 2
+split_input = strsplit(input, '')
+
+sapply(split_input, \(x) {
+  sort(sapply(split_input, \(y) {
+    ifelse(sum(y == x) == length(x) - 1, paste(y[y == x], collapse=''), NA)
+  }))
+}) |> 
+  unlist()
