ceb4269cfa59c8a56705cc26cbd638432997fbf2
Aron Strandberg <aron.olof.strandberg@gmail.com>
2022-11-29 20:50:33 +0100

    Modify part 1, add part 2 solution

--- 2018/scripts/day-05.R
+++ 2018/scripts/day-05.R
@@ -2,31 +2,30 @@
 
 input <- readLines('2018/input/input-05')
 
-# Part 1 
-reaction <- \(v, i = 0, no_change = F) {
-  compare = v[pmin(pmax(seq(v) + sign((i+seq(v)) %% 2-.5), 1), max(seq(v)))]
-  new_v <- v[abs(v - compare) != 32]
-  if(no_change & identical(v, new_v)) return(new_v)
-  Recall(new_v, i+1, identical(v, new_v))
+# Part 1
+reaction <- function(input) {
+  v <- utf8ToInt(input)
+  i <- 0
+  while(TRUE) {
+    compare = v[pmin(pmax(seq(v) + sign((i+seq(v)) %% 2-.5), 1), max(seq(v)))]
+    new_v <- v[abs(v - compare) != 32]
+    if(identical(v, new_v)) return(nchar(intToUtf8(v)))
+    v <- new_v
+    i <- i + 1
+  }
 }
+reaction(input)
 
-nchar(intToUtf8(reaction(utf8ToInt(input))))
-
-# Part 2
-shortest = nchar(intToUtf8(reaction(utf8ToInt(input))))
-for(x in 1:26) {
-  removed <- gsub(paste(letters[x], LETTERS[x], sep = '|'), '', input)
-  shortest <- min(
-    shortest,
-    nchar(intToUtf8(reaction(utf8ToInt(removed))))
-  )
-}
-shortest
-
+# Part 2 apply
 sapply(1:26, \(x) {
-  utf8ToInt(gsub(paste(letters[x], LETTERS[x], sep = '|'), '', input)) |>
-    reaction() |>
-    intToUtf8() |>
-    nchar()
-})
+  reaction(gsub(paste(letters[x], LETTERS[x], sep = '|'), '', input))
+  }) |>
+  min()
 
+# Part 2 loop
+shortest <- c()
+for(x in 1:26) {
+  shortest[x] <- reaction(gsub(paste(letters[x], LETTERS[x], sep = '|'), '', input))
+  print(shortest[x])
+}
+min(shortest)
