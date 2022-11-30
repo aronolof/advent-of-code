ee317c42b2095e8d598ada7195c597c47ca81c14
Aron Strandberg <aron.olof.strandberg@gmail.com>
2022-11-30 14:18:07 +0100

    Add day 6 solution

---
+++ 2018/scripts/day-06.R
@@ -0,0 +1,27 @@
+# --- Day 6: Chronal Coordinates ---
+
+input <- read.csv('2018/input/input-06', header = F) 
+
+# Part 1
+space <- matrix(NA, nrow = max(input$V2), ncol = max(input$V1))
+
+for(y in 1:nrow(space)) {
+  for(x in 1:ncol(space)) {
+    nearest <- rownames(input)[rank(abs(input$V1-x)+abs(input$V2-y)) == 1]
+    space[y, x] <- ifelse(length(nearest) == 0, '0', nearest)
+  }
+}
+
+infinite <- unique(c(0, space[c(1, nrow(space)),], space[,c(1, ncol(space))]))
+areas <- sort(table(space), TRUE)
+areas[setdiff(names(areas), infinite)][1]
+
+# Part 2
+space <- matrix(NA, nrow = max(input$V2), ncol = max(input$V1))
+
+for(y in 1:nrow(space)) {
+  for(x in 1:ncol(space)) {
+    space[y, x] <- sum(abs(input$V1-x) + abs(input$V2-y)) < 10000
+  }
+}
+sum(space)
