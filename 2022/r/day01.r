ca38f8d37422be2a577351c97e8316aa96b94789
Aron Strandberg <aron.olof.strandberg@gmail.com>
2022-12-01 10:46:47 +0100

    Formatting

--- 2022/r/day01.r
+++ 2022/r/day01.r
@@ -3,7 +3,7 @@
 input <- as.numeric(readLines("2022/data/input.txt"))
 
 # Part 1
-sort(sapply(split(input, cumsum(is.na(input))), sum, na.rm=T), TRUE)[1]
+sort(sapply(split(input, cumsum(is.na(input))), sum, na.rm = T), T)[1]
 
 # Part 2
-sum(sort(sapply(split(input, cumsum(is.na(input))), sum, na.rm=T), TRUE)[1:3])
+sum(sort(sapply(split(input, cumsum(is.na(input))), sum, na.rm = T), T)[1:3])
