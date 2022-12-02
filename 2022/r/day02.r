a3529d46d07228bdbc044f2ce455542236ba07e8
Aron Strandberg <aron.olof.strandberg@gmail.com>
2022-12-02 15:55:25 +0100

    Add day 2 solution

---
+++ 2022/r/day02.r
@@ -0,0 +1,28 @@
+# --- Day 2: Rock Paper Scissors ---
+
+input <- read.table("2022/data/input02.txt", sep=" ")
+
+# Part 1
+apply(input, 1, \(x) {
+  
+    opponent = setNames(1:3, c('A','B','C'))[x[1]]
+    choice   = setNames(1:3, c('X','Y','Z'))[x[2]]
+    
+    c(3, 6, 0)[1 + (choice - opponent) %% 3] + choice
+  }) |>
+  sum()
+
+# Part 2
+apply(input, 1, \(x) {
+  
+    opponent = setNames(1:3, c('A','B','C'))[x[1]]
+    strategy = setNames(1:3, c('X','Y','Z'))[x[2]]
+    choice = 1 + (opponent + strategy) %% 3
+    
+    c(0, 3, 6)[strategy] + choice
+  }) |>
+  sum()
+
+
+
+
