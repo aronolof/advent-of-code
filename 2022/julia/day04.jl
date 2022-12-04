b92630927143047b4e3f0a4d8c94e46448722afa
Aron Strandberg <aron.olof.strandberg@gmail.com>
2022-12-04 09:06:25 +0100

    Add julia day 4 solution

---
+++ 2022/julia/day04.jl
@@ -0,0 +1,20 @@
+# --- Day 4: Camp Cleanup ---
+
+parse_input = function(input_path)
+    input = split.(readlines(input_path), (['-',','],))
+    input_int = [parse.(Int, x) for x in input]
+    return [[col[i] for col in input_int] for i in 1:4]
+end
+
+part01 = function(input_path)
+    a, b, c, d  = parse_input(input_path)
+    sum((a .>= c) .& (b .<= d) .| (a .<= c) .& (b .>= d))
+end
+
+part02 = function(input_path)
+    a, b, c, d  = parse_input(input_path)
+    sum((b .>= c) .& (b .<= d) .| (a .<= d) .& (b .>= d))
+end
+
+println("Answer 1: ", part01("2022/data/input04.txt"))
+println("Answer 2: ", part02("2022/data/input04.txt"))
