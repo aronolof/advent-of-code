ce2a765a335db7a2e65dc02fb13c557489d9c9ba
Aron Strandberg <aron.olof.strandberg@gmail.com>
2022-12-04 07:20:02 +0100

    Add day 4 solution

---
+++ 2022/r/day04.r
@@ -0,0 +1,8 @@
+# --- Day 4: Camp Cleanup ---
+x <- read.table(text = gsub("-|,", " ", readLines("2022/data/input04.txt")))
+
+# Part 1
+sum((x[1] >= x[3] & x[2] <= x[4]) | (x[1] <= x[3] & x[2] >= x[4]))
+
+# Part 2
+sum((x[2] >= x[3] & x[2] <= x[4]) | (x[1] <= x[4] & x[2] >= x[4]))
