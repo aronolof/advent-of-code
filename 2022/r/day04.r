cf04f9a217f5e8528f038eb270f1751831a968d8
Aron Strandberg <aron.olof.strandberg@gmail.com>
2022-12-05 01:03:11 +0100

    Simply day 4 solution a bit

--- 2022/r/day04.r
+++ 2022/r/day04.r
@@ -5,4 +5,4 @@ x <- read.table(text = gsub("-|,", " ", readLines("2022/data/input04.txt")))
 sum((x[1] >= x[3] & x[2] <= x[4]) | (x[1] <= x[3] & x[2] >= x[4]))
 
 # Part 2
-sum((x[2] >= x[3] & x[2] <= x[4]) | (x[1] <= x[4] & x[2] >= x[4]))
+sum((x[2] >= x[3] & x[1] <= x[4]))
