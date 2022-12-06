e8e1dcecdd0431349ab5df70f36f5429cdef347c
Aron Strandberg <aron.olof.strandberg@gmail.com>
2022-12-06 06:26:55 +0100

    Add day 6 solution

---
+++ 2022/r/day06.r
@@ -0,0 +1,12 @@
+input <- strsplit(readLines("2022/data/input06.txt"), '')[[1]]
+
+find_marker <- \(input, size) {
+  for(i in tail(seq(input), 1-size)) {
+    if(length(unique(input[(i-size+1):i])) == size) return(i)
+  }
+}
+
+# Part 1
+find_marker(input, 4)
+# Part 2
+find_marker(input, 14)
