6917f1fd7bbd552ec84fd34a80fa69c6525ef24c
Aron Strandberg <aron.olof.strandberg@gmail.com>
2022-12-09 14:39:34 +0100

    Additional linting

--- 2022/r/day09.r
+++ 2022/r/day09.r
@@ -4,7 +4,7 @@ input <- readLines("2022/data/input09.txt") |>
 
 # Part 1
 hpos <- c(0, 0)
-tpos = c(0, 0)
+tpos <- c(0, 0)
 visited <- list(tpos)
 
 for (move in input) {
@@ -14,7 +14,7 @@ for (move in input) {
     if (move[1] == 'U') hpos <- hpos + c(0, -1)
     if (move[1] == 'D') hpos <- hpos + c(0, 1)
     
-    if (max(abs(tpos - hpos)) > 1) tpos = tpos + sign(hpos - tpos)
+    if (max(abs(tpos - hpos)) > 1) tpos <- tpos + sign(hpos - tpos)
     
     visited <- append(visited, list(tpos))
   }
@@ -27,13 +27,13 @@ visited <- rope[10]
 
 for (move in input) {
   for (step in seq(as.numeric(move[2]))) {
-    if (move[1] == 'R') rope[[1]] = rope[[1]] + c(1, 0)
-    if (move[1] == 'L') rope[[1]] = rope[[1]] + c(-1, 0)
-    if (move[1] == 'U') rope[[1]] = rope[[1]] + c(0, -1)
-    if (move[1] == 'D') rope[[1]] = rope[[1]] + c(0, 1)
+    if (move[1] == 'R') rope[[1]] <- rope[[1]] + c(1, 0)
+    if (move[1] == 'L') rope[[1]] <- rope[[1]] + c(-1, 0)
+    if (move[1] == 'U') rope[[1]] <- rope[[1]] + c(0, -1)
+    if (move[1] == 'D') rope[[1]] <- rope[[1]] + c(0, 1)
     
-    for(i in seq(rope)[-1]) {
-      if (max(abs(rope[[i]] - rope[[i-1]])) > 1) {
+    for (i in seq(rope)[-1]) {
+      if (max(abs(rope[[i]] - rope[[i - 1]])) > 1) {
         rope[[i]] <- rope[[i]] + sign(rope[[i - 1]] - rope[[i]])
       }
     }
