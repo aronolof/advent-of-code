a2c7079132323e875b7810aee7385a838c34eed0
Aron Strandberg <aron.olof.strandberg@gmail.com>
2022-12-10 07:38:19 +0100

    Clean up solution

--- 2022/r/day10.r
+++ 2022/r/day10.r
@@ -3,16 +3,22 @@ input <- readLines("2022/data/input10.txt") |>
   strsplit(' ')
 
 # Part 1
-cycles <- c(1)
+cycle <- 1
+register <- 1
 for (i in seq(input)) {
-  if (input[[i]][1] == 'noop') cycles <- c(cycles, tail(cycles, 1))
-  if (input[[i]][1] == 'addx') cycles <- c(cycles, tail(cycles, 1) + c(0, as.numeric(input[[i]][2])))
+  if (input[[i]][1] == 'noop') {
+    cycle <- cycle + 1
+    register[cycle] <- tail(register, 1)
+  } else if (input[[i]][1] == 'addx') {
+    cycle <- cycle + 2
+    register[cycle + c(-1, 0)] <- tail(register, 1) + c(0, as.numeric(input[[i]][2]))
+  }
 }
-sapply(seq(20, length(cycles), 40), \(i) cycles[i] * i) |>
+sapply(seq(20, length(register), 40), \(i) register[i] * i) |>
   sum()
 
 # Part 2
-pos <- (seq(cycles) - 1) %% 40
-draw <- abs(pos - cycles) <= 1
+pos <- (seq(register) - 1) %% 40
+draw <- abs(pos - register) <= 1
 img <- matrix(head(draw, -1), ncol = 40, byrow = T) 
 image(t(img[nrow(img):1,]))
