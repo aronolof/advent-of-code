5b1ac1a038454c76a20376fcbe093185425b90f1
Aron Strandberg <aron.olof.strandberg@gmail.com>
2022-12-11 08:38:36 +0100

    Add part 2 solution

--- 2022/r/day11.r
+++ 2022/r/day11.r
@@ -1,43 +1,84 @@
 # --- Day 11: Monkey in the Middle ---
-
 input <- readLines("2022/data/input11.txt") 
-input_list <- split(input, cumsum(grepl('Monkey', input)))
 
-state <- lapply(input_list, \(x) {
-  list(n_inspections = 0,
-       items = as.numeric(strsplit(gsub('  Starting items: ', '', x[2]), ',')[[1]]),
-       operation = gsub('  Operation: new = ', '', x[3]),
-       test = as.integer(gsub('  Test: divisible by ', '', x[4])),
-       true = as.integer(gsub('    If true: throw to monkey ', '', x[5])),
-       false = as.integer(gsub('    If false: throw to monkey ', '', x[6])))
-})
+initial_state <- function(input) {
+  split(input, cumsum(grepl('Monkey', input))) |>
+    lapply(\(x) {
+      list(n_inspections = 0,
+           items = as.numeric(strsplit(gsub('  Starting items: ', '', x[2]), ',')[[1]]),
+           operation = gsub('  Operation: new = ', '', x[3]),
+           test = as.integer(gsub('  Test: divisible by ', '', x[4])),
+           true = as.integer(gsub('    If true: throw to monkey ', '', x[5])),
+           false = as.integer(gsub('    If false: throw to monkey ', '', x[6])))
+    })
+}
+# Part 1
+state <- initial_state(input)
 
 for (round in 1:20) {
+  if (round %% 50 == 0) print(round)
   for (i in seq(state)) {
-    print(paste(round, i))
+    
     if (length(state[[i]]$items) > 0) {
       for (j in seq(length(state[[i]]$items))) {
         
         item = state[[i]]$items[j]
-        new_worry <- gsub('old', as.character(item), state[[i]]$operation) |>
+        worry_level <- gsub('old', as.character(item), state[[i]]$operation) |>
           parse(text = _) |>
           eval()
         
-        new_worry <- new_worry %/% 3
-        is_divisible <- (new_worry %% state[[i]]$test) == 0
+        worry_level <- worry_level %/% 3
+        is_divisible <- (worry_level %% state[[i]]$test) == 0
         
         if (is_divisible) {
           throw_to <- state[[i]]$true + 1
         } else {
           throw_to <- state[[i]]$false + 1
         }
-        state[[throw_to]]$items <- append(state[[throw_to]]$items, new_worry)
+        state[[throw_to]]$items <- append(state[[throw_to]]$items, worry_level)
         state[[i]]$n_inspections <- state[[i]]$n_inspections + 1
       }
       state[[i]]$items <- NULL
     }
   }
 }
-# Part 1
-sapply(seq(state), \(i) state[[i]][[1]]) |> sort(decreasing = T) |> head(2) |> prod()
+sapply(state, \(x) x$n_inspections) |>
+  sort() |>
+  tail(2) |>
+  prod()
 
+# Part 2
+state <- initial_state(input)
+lcd <- prod(sapply(state, \(x) x$test))
+
+for (round in seq(10000)) {
+  if (round %% 100 == 0) print(round)
+  for (i in seq(state)) {
+    
+    if (length(state[[i]]$items) > 0) {
+      for (j in seq(length(state[[i]]$items))) {
+        
+        item = state[[i]]$items[j]
+        worry_level <- gsub('old', as.character(item), state[[i]]$operation) |>
+          parse(text = _) |>
+          eval()
+        
+        worry_level <- worry_level %% lcd
+        is_divisible <- (worry_level %% state[[i]]$test) == 0
+        
+        if (is_divisible) {
+          throw_to <- state[[i]]$true + 1
+        } else {
+          throw_to <- state[[i]]$false + 1
+        }
+        state[[throw_to]]$items <- append(state[[throw_to]]$items, worry_level)
+        state[[i]]$n_inspections <- state[[i]]$n_inspections + 1
+      }
+      state[[i]]$items <- NULL
+    }
+  }
+}
+sapply(state, \(x) x$n_inspections) |>
+  sort() |>
+  tail(2) |>
+  prod()
