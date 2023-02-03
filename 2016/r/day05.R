0a4e4f497e7d62acf27ba069c4040703882de8d3
Aron Strandberg <aron.olof.strandberg@gmail.com>
2023-02-03 03:07:36 +0100

    2016 day 5 part 2

--- 2016/r/day05.R
+++ 2016/r/day05.R
@@ -1,8 +1,10 @@
 # --- Day 5: How About a Nice Game of Chess? ---
 library(digest)
 
-password <- c()
+input <- 'uqwqemis'
 
+# Part 1
+password <- c()
 i <- 0
 while (TRUE) {
   hash <- paste0(input, i) |>
@@ -22,4 +24,30 @@ while (TRUE) {
 }
 paste(password, collapse = '')
 
-
+# Part 2
+password <- rep(NA, 8)
+i <- 0
+while (TRUE) {
+  hash <- paste0(input, i) |>
+    digest(algo = 'md5', serialize = FALSE)
+  
+  if (substr(hash, 1, 5) == '00000') {
+    pos <- substr(hash, 6, 6)
+    
+    if (pos %in% as.character(0:7)) {
+      pos <- as.numeric(pos) + 1
+      if (is.na(password[pos])) {
+        password[pos] <- substr(hash, 7, 7)
+      }
+      print(password)
+    }
+  }
+  if (sum(is.na(password)) == 0) {
+    break
+  }
+  if (i %% 100000 == 0) {
+    print(i)
+  }
+  i <- i + 1
+}
+paste(password, collapse = '')
