b63536e88cda4833ef7453c2a9ec6c44cbc2dc45
Aron Strandberg <aron.olof.strandberg@gmail.com>
2023-02-10 16:00:00 +0100

    2016 day 7 part 2

--- 2016/r/day07.R
+++ 2016/r/day07.R
@@ -10,11 +10,35 @@ input |>
     
     abba <- sapply(y, \(z) {
       sapply(seq(4, length(z)), \(i){
-        z[i] == z[i-3] & z[i-1] == z[i-2] & z[i] != z[i-1]
+        z[i] == z[i - 3] & z[i - 1] == z[i - 2] & z[i] != z[i - 1]
       }) |>
         any()
     })
     
     any(abba[c(T, F)]) & !any(abba[c(F, T)])
+  }) |>
+  sum()
+
+# Part 2
+input |>
+  strsplit('\\[|\\]') |>
+  sapply(\(x) {
+    
+    bab <- x[(seq(x) %% 2) == 0] |>
+             paste0(collapse = '|')
+    
+    aba <- x[(seq(x) %% 2) == 1] |>
+      paste('') |>
+      strsplit('') |>
+      unlist()
+    
+    for (i in seq(3, length(aba))) {
+      if (aba[i - 2] == aba[i] && aba[i - 1] != aba[i]) {
+        if (grepl(paste(aba[c(i - 1, i, i - 1)], collapse = ''), bab)) {
+          return(TRUE)
+        }
+      } 
+    }
+    return(FALSE)
   }) |>
   sum()
