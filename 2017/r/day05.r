97834eb9ed3c30cac8f82f242414a0ac793f5ae1
Aron Strandberg <aron.olof.strandberg@gmail.com>
2023-02-13 00:15:27 +0100

    2017 day 5 solution

--- 2017/r/day05.r
+++ 2017/r/day05.r
@@ -1,17 +1,27 @@
 # --- Day 5: A Maze of Twisty Trampolines, All Alike ---
-
 input <- scan("2017/data/input05.txt")
 
 # Part 1
-maze <- function(x) {
+maze <- function(x, step2 = FALSE) {
   i <- 1
   n_steps <- 0
-  while(TRUE) {
+  while (TRUE) {
     n_steps <- n_steps + 1
-    x[i] <- x[i] + 1
     j <- i
-    i <- i + x[i] - 1
-    if(i > length(x)) return(n_steps)
+    i <- i + x[i]
+    
+    if (step2 && x[j] >= 3) {
+      x[j] <- x[j] - 1
+    } else {
+      x[j] <- x[j] + 1
+    }
+    
+    if (i > length(x)) {
+      return(n_steps)
+    }
   }
 }
 maze(input)
+
+# Part 2
+maze(input, step2 = TRUE)
