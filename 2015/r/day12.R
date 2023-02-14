08e31f68dd7fca05ca46e53aedc05a13f4b911ae
Aron Strandberg <aron.olof.strandberg@gmail.com>
2023-02-14 03:14:38 +0100

    2015 day 12 part 2

--- 2015/r/day12.R
+++ 2015/r/day12.R
@@ -3,8 +3,41 @@
 input <- readLines('2015/data/input12.txt')
 
 # Part 1
-gsub('[^0-9-]+', ',', input) |>
-  strsplit(',') |>
-  unlist() |>
-  as.numeric() |>
-  sum(na.rm = TRUE)
+sum_of_numbers <- function(input) {
+  gsub('[^0-9-]+', ',', input) |>
+    strsplit(',') |>
+    unlist() |>
+    as.numeric() |>
+    sum(na.rm = TRUE)
+}
+
+sum_of_numbers(input)
+
+# Part 2
+ignore_red <- function(input) {
+  chars <- strsplit(input, '')[[1]]
+  bracket <- c(']' = '[', '}' = '{')
+  stack <- c()
+  backtracking <- c()
+  for (char in chars) {
+    stack <- append(stack, char)
+    
+    if (char %in% names(bracket)) {
+      while (TRUE) {
+        backtracking <- append(backtracking, tail(stack, 1))
+        stack <- head(stack, -1)
+        if (tail(backtracking, 1) == bracket[char]) {
+          break
+        }
+      }
+      backtracking <- paste(rev(backtracking), collapse = '')
+      if (char == ']' | !grepl('red', backtracking)) {
+        stack <- append(stack, gsub('red', '', backtracking))
+      }
+      backtracking <- c()
+    }
+  }
+  sum_of_numbers(stack)
+}
+
+ignore_red(input)
