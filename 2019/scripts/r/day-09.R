8ff4a0ed7969721b5db7c94128437b1041a3f61f
Aron Strandberg <aron.olof.strandberg@gmail.com>
2023-02-15 17:22:59 +0100

    Move day 10, start on day 9

--- 2019/scripts/r/day-09.R
+++ 2019/scripts/r/day-09.R
@@ -1,35 +1,88 @@
-# --- Day 10: Monitoring Station ---
+# --- Day 9: Sensor Boost ---
+program <- scan("2019/input/input-09.txt", sep = ",")
 
-library(tidyverse)
+# My brand new Intcode computer
+run_prog <- function(prog, input = 0) {
+  relative_base = 0
+  i <- 1
+  
+  while (TRUE) {
+    op <- prog[i] %% 100
+    mode <- sapply(2:4, \(x) (prog[i] %% (10^(x + 1))) %/% (10^x))
+    
+    par <- function(n) {
+      if (mode[n] == 0) {
+        val <- prog[prog[i + n] + 1]
+      } else if (mode[n] == 1) {
+        val <- prog[i + n]
+      } else if (mode[n] == 2) {
+        val <- prog[prog[i + n] + 1 + relative_base]
+      } else {
+        stop("Mode error")
+      }
+      ifelse(is.na(val), 0, val)
+    }
+    
+    if (op %in% c(1:2, 7:8)) {
+      # params = 3
+      prog[prog[i + 3] + 1] <- do.call(c("sum", "prod", "<", "==")[[which(op == c(1,2,7,8))]],
+                                       list(par(1), par(2)))
+      if (prog[i + 3] + 1 == i) {
+        i <- prog[i + 3] + 1
+      } else {
+        i <- i + 4
+      }
+      
+    } else if (op == 3) {
+      # params = 1
+      if (mode[1] == 0) prog[prog[i + 1] + 1] <- input
+      if (mode[1] == 2) {
+        print(i)
+        print(op)
+        print(prog[i])
+        print(prog[i + 1])
+        prog[prog[i + 1] + 1 + relative_base] <- input
+        stop("STOP")
+      }
+      
+      if (prog[i + 1] + 1 == i) {
+        i <- prog[i + 1] + 1
+      } else {
+        i <- i + 2
+      }
+      
+    } else if (op == 4) {
+      # params = 1
+      if (mode[1] == 0) print(as.character(prog[prog[i + 1] + 1]))
+      if (mode[1] == 1) print(as.character(prog[i + 1]))
+      if (mode[1] == 2) print(as.character(prog[prog[i + 1] + 1 + relative_base]))
 
-input <- scan("2019/input/input-10.txt",  what = "character")
+      i <- i + 2
+      
+    } else if (op %in% 5:6) {
+      # params = 2
+      if (do.call(c("!=", "==")[[which(op == c(5:6))]], list(par(1), 0))) {
+        i <- par(2) + 1
+      } else {
+        i <- i + 2 + 1
+      }
+    } else if (op == 9) {
+      # params = 1
+      relative_base <- relative_base + par(1)
+      i <- i + 2
+    } else if (op == 99) {
+      break
+    } else {
+      stop("Invalid opcode")
+    }
+  }
+}
 
-asteroid_pairs <- input %>% 
-  str_split("", simplify = TRUE) %>%
-  {as_tibble(cbind(id = c(.), x=c(col(.)-1), y=c(row(.)-1)))} %>%
-  filter(id == "#") %>%
-  mutate(dummy = 1, id = row_number()) %>%
-  full_join(rename_all(., ~ paste0(., "2")), c("dummy" = "dummy2")) %>%
-  mutate_all(as.numeric) %>%
-  mutate(angle = atan2(y - y2, x - x2)) %>%
-  filter(!(x == x2 & y == y2))
+c(104,1125899906842624,99) |>
+  run_prog()
 
 # Part 1
-asteroid_pairs %>%
-  group_by(id) %>%
-  summarise(n_detected = n_distinct(angle)) %>%
-  filter(n_detected == max(n_detected))
+run_prog(program, 1)
 
 # Part 2
-asteroid_pairs %>%
-  filter(id == 213) %>%
-  mutate(dist = sqrt((x - x2) ^ 2 + (y - y2) ^ 2)) %>%
-  mutate(angle = (angle + pi + pi / 2) %% (2 * pi)) %>%
-  arrange(angle, dist) %>%
-  group_by(angle) %>%
-  mutate(angle_n = row_number()) %>%
-  arrange(angle_n, angle) %>%
-  ungroup() %>%
-  mutate(row_number = row_number()) %>%
-  filter(row_number == 200) %>% 
-  {.$x2 * 100 + .$y2}
+run_prog(input, 5)
---
+++ 2019/scripts/r/day-10.R
@@ -0,0 +1,35 @@
+# --- Day 10: Monitoring Station ---
+
+library(tidyverse)
+
+input <- scan("2019/input/input-10.txt",  what = "character")
+
+asteroid_pairs <- input %>% 
+  str_split("", simplify = TRUE) %>%
+  {as_tibble(cbind(id = c(.), x=c(col(.)-1), y=c(row(.)-1)))} %>%
+  filter(id == "#") %>%
+  mutate(dummy = 1, id = row_number()) %>%
+  full_join(rename_all(., ~ paste0(., "2")), c("dummy" = "dummy2")) %>%
+  mutate_all(as.numeric) %>%
+  mutate(angle = atan2(y - y2, x - x2)) %>%
+  filter(!(x == x2 & y == y2))
+
+# Part 1
+asteroid_pairs %>%
+  group_by(id) %>%
+  summarise(n_detected = n_distinct(angle)) %>%
+  filter(n_detected == max(n_detected))
+
+# Part 2
+asteroid_pairs %>%
+  filter(id == 213) %>%
+  mutate(dist = sqrt((x - x2) ^ 2 + (y - y2) ^ 2)) %>%
+  mutate(angle = (angle + pi + pi / 2) %% (2 * pi)) %>%
+  arrange(angle, dist) %>%
+  group_by(angle) %>%
+  mutate(angle_n = row_number()) %>%
+  arrange(angle_n, angle) %>%
+  ungroup() %>%
+  mutate(row_number = row_number()) %>%
+  filter(row_number == 200) %>% 
+  {.$x2 * 100 + .$y2}
