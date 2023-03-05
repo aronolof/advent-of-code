6b22a7de885683de03ed3b0ef41c6972ceb65274
Aron Strandberg <aron.olof.strandberg@gmail.com>
2023-03-05 17:21:02 +0100

    2015 day 17 part 2

--- 2015/r/day17.R
+++ 2015/r/day17.R
@@ -2,25 +2,41 @@
 
 input <- scan('2015/data/input17.txt')
 
-find_combinations <- function(containers = input, fit = 150) {
+find_combinations <- function(containers = input, fit = 150, part_2 = FALSE) {
   containers <- containers |>
     sort(decreasing = TRUE)
   
-  try_combinations <- function(containers, amount) {
-    if (amount == fit) {
+  combinations <- list()
+  
+  try_combinations <- function(containers, used) {
+    if (sum(used) == fit) {
+      if (part_2) {
+        combinations <<- append(combinations, list(used))
+      }
       return(1)
     }
-    if (length(containers) <= 0 || amount > fit) return(0)
+    if (length(containers) <= 0 || sum(used) > fit) return(0)
     
-    combinations <- 0
+    n_combinations <- 0
     for (i in seq_along(containers)) {
-      combinations <- combinations + try_combinations(tail(containers, -i), amount + containers[i])
+      n_combinations <- n_combinations + try_combinations(tail(containers, -i), c(used, containers[i]))
     }
-    return(combinations)
+    return(n_combinations)
   }
   
-  try_combinations(containers, amount = 0)
+  n_combinations <- try_combinations(containers, NULL)
+  
+  if (part_2) return(combinations)
+  
+  return(n_combinations)
+  
 }
 
 # Part 1
 find_combinations(input, 150)
+
+# Part 2
+find_combinations(input, 150, part_2 = TRUE) |>
+  sapply(length) |>
+  table() |>
+  head(1)
