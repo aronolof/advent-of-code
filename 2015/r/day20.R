e5fe63a2e166a2d662b4c484dc6cd1c359ed72bf
Aron Strandberg <aron.olof.strandberg@gmail.com>
2023-03-11 02:52:37 +0800

    2015 day 20 part 2

--- 2015/r/day20.R
+++ 2015/r/day20.R
@@ -1,26 +1,23 @@
 # --- Day 20: Infinite Elves and Infinite Houses ---
 
-find_lowest <- function(n) {
+find_lowest <- function(n, multiplier = 10, max_n_presents = Inf) {
   
-  # Find a reasonable maxn
-  maxn <- n
-  for (i in 50:5) {
-    gifts <- sum(which(((n %/% i) %% seq_len(n %/% i)) == 0)) * 10
-    if (gifts >= n) {
-      maxn <- n %/% i
-      break
-    }
-  }
-  
-  divisors = rep(0, maxn)
+  max_n <- max(1, n %/% multiplier)
+  divisors = rep(0, max_n)
 
-  for (i in seq(1, maxn)) {
-    for (j in seq(i, maxn, i)) {
-      divisors[j] <- divisors[j] + i * 10
-      if (i == j && divisors[j] >= n) return(j)
+  for (i in seq(1, max_n)) {
+    for (j in head(seq(i, max_n, i), max_n_presents)) {
+      divisors[j] <- divisors[j] + i * multiplier
+      
+      if (i == j && divisors[j] >= n) {
+        return(j)
+      }
     }
   }
 }
 
 # Part 1
-find_lowest(331000)
+find_lowest(33100000, multiplier = 10)
+
+# Part 2
+find_lowest(33100000, multiplier = 11, max_n_presents = 50)
