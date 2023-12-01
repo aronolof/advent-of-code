39a8676a4f1683e227d62b7aaa27560100107b6b
Aron Strandberg <aron.strandberg@gmail.com>
2023-12-01 16:20:05 +0100

    Day 1

--- 2023/r/day01.R
+++ 2023/r/day01.R
@@ -1,7 +1,24 @@
-# --- Day 1: ---
+# --- Day 1: Trebuchet?! ---
 
 input <- readLines("2023/data/input01.txt")
 
 # Part 1
+gsub('\\D', '', input) |>
+  sapply(\(x) paste0(substr(x, 1, 1), substring(x, nchar(x)))) |>
+  as.numeric() |>
+  sum()
 
 # Part 2
+d <- c('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', '#', 1:9)
+
+sapply(input, \(x) {
+  matches <- sapply(d, gregexpr, x)
+  
+  first = sapply(matches, min)
+  first[first == -1] <- 99
+  
+  last = sapply(matches, max)
+  
+  sum(10 * which.min(first) %% 10, which.max(last) %% 10)
+}) |>
+  sum()
