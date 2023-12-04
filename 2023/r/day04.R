573b3ab55bb9e34d32736bfcbbe2f2fadba1cc52
Aron Strandberg <aron.strandberg@gmail.com>
2023-12-04 13:18:27 +0100

    delete new line

--- 2023/r/day04.R
+++ 2023/r/day04.R
@@ -3,13 +3,18 @@
 input <- readLines("2023/data/input04.txt")
 
 # Part 1
-input |>
-  strsplit(' \\| ') |>
+wins <- strsplit(input, ' \\| ') |>
   sapply(\(x) {
     card <- strsplit(x, ' +')
-    win <- sum(card[[1]] %in% card[[2]])
-    (win >= 1) * (2 ^ (win - 1))
-  }) |>
-  sum()
+    sum(card[[1]] %in% card[[2]])
+  })
+sum((wins >= 1) * (2 ^ (wins - 1)))
 
 # Part 2
+n_cards <- wins ^ 0
+for (i in seq(n_cards)) {
+  if(wins[i] > 0) {
+    n_cards[i + seq(wins[i])] <- n_cards[i + seq(wins[i])] + n_cards[i]
+  }
+}
+sum(n_cards)
